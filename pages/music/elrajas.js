81cc5f2e3d833382d01c9a2dc2b94071d8ec54dd
Daniel Schmitz <danielschmitz.arts@gmail.com>
2021-01-02 09:48:51 +0100

    Trimurti Artists adjustments

--- pages/music/elrajas.js
+++ pages/music/elrajas.js
@@ -6,11 +6,12 @@ import SocialIcon from "../components/SocialIcon";
 const Index = () => (
     <MusicPage>
         <Title page="Elrajas"/>
-        <h1>Nitamas</h1>
-        <h2 className="subtitle">Organica & Tribal Techno</h2>
+        <h1>Elrajas</h1>
+        <h2 className="subtitle">Organica, Tribal and Cyber Techno</h2>
         <div className="description">
             <img src="/img/music/elrajas.jpg"/>
             <div className="socialIcons">
+                <SocialIcon spotify="https://open.spotify.com/artist/1Dtnpnv1YJ5sMz2cV1yOy6?si=7jb9q03oQWab7GNoHIyueQ" size="2x"/>
                 <SocialIcon soundcloud="https://www.soundcloud.com/elrajas" size="2x"/>
                 <SocialIcon mixcloud="https://www.mixcloud.com/elrajas" size="2x"/>
                 <SocialIcon instagram="https://www.instagram.com/elrajasmusic" size="2x"/>
